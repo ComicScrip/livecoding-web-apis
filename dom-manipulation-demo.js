461a3a1d163d60d7c42e0cb1c7f6d760dfebe283
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2021-09-27 20:52:42 +0200

    localstorage with tasks

--- dom-manipulation-demo.js
+++ dom-manipulation-demo.js
@@ -1,26 +1,76 @@
+// retrieve DOM elements
 const taskFrom = document.getElementById('task-form');
 const taskNameInput = document.getElementById('task-name-input');
 const taskList = document.getElementById('task-list');
 const resetDoneTaskBtn = document.getElementById('reset-all-done-task-btn');
 
-function handleTaskClick(event) {
-  const taskElement = event.target;
-  taskElement.classList.toggle('task-done');
+// load saved tasks from localStorage
+const savedTasks = JSON.parse(localStorage.getItem('savedTasks')) || [];
+
+// add saved tasks to the DOM when the app starts
+for (let i = 0; i < savedTasks.length; i++) {
+  addTask(savedTasks[i]);
 }
 
-taskFrom.addEventListener('submit', function (event) {
-  event.preventDefault();
-  const taskItemElement = document.createElement('li');
-  taskItemElement.innerText = taskNameInput.value;
-  taskItemElement.addEventListener('click', handleTaskClick);
-  taskList.appendChild(taskItemElement);
-  taskNameInput.value = '';
-});
+function persistInMemoryTasks() {
+  localStorage.setItem('savedTasks', JSON.stringify(savedTasks));
+}
 
-resetDoneTaskBtn.addEventListener('click', function () {
+function createTask(name) {
+  const newTask = { name: name, done: false };
+  // save to memory
+  savedTasks.push(newTask);
+  // save to storage
+  persistInMemoryTasks();
+  return newTask;
+}
+
+function forgetDoneTasks() {
+  // remove from memory
+  for (let i = 0; i < savedTasks.length; i++) {
+    if (savedTasks[i].done) savedTasks.splice(i, 1);
+  }
+  // remove from storage
+  persistInMemoryTasks();
+}
+
+function addTask(task) {
+  const taskItemElement = document.createElement('li');
+  function toggleDone() {
+    taskItemElement.classList.toggle('task-done');
+    task.done = !task.done;
+    persistInMemoryTasks();
+  }
+  taskItemElement.innerText = task.name;
+  if (task.done) {
+    taskItemElement.classList.add('task-done');
+  }
+  taskItemElement.addEventListener('click', toggleDone);
+  taskList.appendChild(taskItemElement);
+}
+
+function removeDoneTasks() {
   const doneTasks = document.querySelectorAll('.task-done');
   for (let i = 0; i < doneTasks.length; i += 1) {
     const doneTask = doneTasks[i];
     doneTask.remove();
   }
+}
+
+taskFrom.addEventListener('submit', function (event) {
+  event.preventDefault();
+  if (taskNameInput.value) {
+    // update data
+    const newTask = createTask(taskNameInput.value);
+    // update the view
+    addTask(newTask);
+    taskNameInput.value = '';
+  }
+});
+
+resetDoneTaskBtn.addEventListener('click', function () {
+  // update data
+  forgetDoneTasks();
+  // update view
+  removeDoneTasks();
 });
